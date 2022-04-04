4da01eed3d2285106f578ccdc51fb15066837ea3
İzel Kayacık <izelkayacik@hotmail.com>
2022-04-04 15:09:25 +0300

    Service
    We are explore the service class, create a service with it and understand the differences to an intent service.

---
+++ app/src/main/java/com/example/servicesexample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.servicesexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStartService.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                startService(it)
+                tvServiceInfo.text = "Service running"
+            }
+        }
+        btnStopService.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                stopService(it)
+                tvServiceInfo.text = "Service stopped"
+            }
+        }
+        btnSendData.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                val dataString = etData.text.toString()
+                it.putExtra("EXTRA_DATA", dataString)
+                startService(it)
+            }
+        }
+    }
+}
